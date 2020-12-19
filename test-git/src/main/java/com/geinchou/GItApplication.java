41522d830e68c67ca85017d348f73eb6978e6d7c
ljz <1609059005@qq.com>
2020-12-19 21:56:25 +0800

    1

---
+++ test-git/src/main/java/com/geinchou/GItApplication.java
@@ -0,0 +1,12 @@
+package com.geinchou;
+
+/**
+ * @Author ：ljz
+ * @Date ：Created in 21:54 2020/12/19
+ * @Description：
+ */
+public class GItApplication {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
